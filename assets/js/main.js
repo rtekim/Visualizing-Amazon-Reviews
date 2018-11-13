e9a8d78c71e7cbdf3ae95b025136a46b966b7832
Andre LaFleur <andre.lafleur@utah.edu>
2018-11-13 13:50:15 -0700

    Finally get jekyll to work on my machine and setup data for the scatterplot

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,7 +6,25 @@
  */
 let grid, table, scatterplot, detailView;
 
-d3.csv("assets/dataset/books_overview.csv").then(data =>{
+d3.csv("assets/dataset/books_overview.csv").then(data => {
+	// We want numbers for all of the pieces of data, so let's go ahead and clean that up
+	// console.log(data.map((d) => {
+	// 	let newDataPoint = { title: d.title };
+
+	// 	data.columns.forEach((col) => {
+	// 		if (col !== 'title') {
+	// 			newDataPoint[col] = parseFloat(d.col);
+	// 		}
+	// 	})
+
+	// 	return newDataPoint; 
+	// }));
+	// data.columns.forEach((col) => {
+	// 	if (col !== 'title') {
+	// 		console.log(col);
+	// 	}
+	// });
+
 	detailView = new DetailView();
     // grid = new Grid(data);
     scatterplot = new Scatterplot(data, grid);
--- assets/js/scatterplot.js
+++ assets/js/scatterplot.js
@@ -46,6 +46,11 @@ class Scatterplot {
             .attr('x2', this.margin.left)
             .attr('y2', this.margin.bottom + this.height);
 
+        this.plotGroup = this.svg.append('g')
+        	.attr('id', 'scatterplot-plot-group')
+			.attr('transform', 'translate(15, ' + this.height + ') scale(1, -1)')
+			.classed('axis', true);
+
         // TODO: Control panel, small set of details on book selected
 
         this.update();
@@ -53,6 +58,50 @@ class Scatterplot {
 
 	/** Updates the scatterplot with all of the various pieces of information*/
 	update() {
-		
+		// Get the data together, along with the maxima and minima
+		let toShow = this.bookData.map((d) => {
+			return { title: d.title, x: d[this.selections.x], y: d[this.selections.y], size: d[this.selections.size]};
+		});
+
+		let limits = this.maximaAndMinimaOf(toShow);
+		let xScale = d3.scaleLinear()
+			.domain([limits.x.min, limits.x.max])
+			.range([this.margin.left, this.margin.left + this.width]);
+		let yScale = d3.scaleLinear()
+			.domain([limits.y.min, limits.y.max])
+			.range([this.margin.bottom, this.margin.bottom + this.height]);
+		let sizeScale = d3.scaleSqrt()
+			.domain([limits.size.min, limits.size.max])
+			.range([2, 10]);
+
+		console.log(sizeScale);
+
+		this.plotGroup.selectAll('circle')
+			.data(toShow)
+			.enter()
+			.append('circle')
+			.attr('cx', (d) => xScale(d.x))
+			.attr('cy', (d) => yScale(d.y))
+			.attr('r', (d) => sizeScale(d.size));
+	}
+
+	/** Helper method. Grabs the maxima and minima of the data to show to the user. */
+	maximaAndMinimaOf(dataToShow) {
+		let limits = { 
+			x: { min: Infinity, max: -Infinity }, 
+			y: { min: Infinity, max: -Infinity }, 
+			size: { min: Infinity, max: -Infinity } 
+		};
+
+		dataToShow.forEach((d) => {
+			if (d.x < limits.x.min) limits.x.min = d.x;
+			if (d.x > limits.x.max) limits.x.max = d.x;
+			if (d.y < limits.y.min) limits.y.min = d.y;
+			if (d.y > limits.y.max) limits.y.max = d.y;
+			if (d.size < limits.size.min) limits.size.min = d.size;
+			if (d.size > limits.size.max) limits.size.max = d.size;
+		});
+
+		return limits;
 	}
 }
