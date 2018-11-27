9d69337d32180a83b9ae4a7eccb546f76fe080de
Andre LaFleur <andre.lafleur@utah.edu>
2018-11-27 16:59:28 -0700

    Add some thigns

---
+++ assets/js/histogram.js
@@ -0,0 +1,15 @@
+/**
+ * histogram.js
+ *
+ * Histogram to be included on the detail view.
+ */
+class Histogram {
+	initialize() {
+
+	}
+
+	/** Update the instagram with the new set of reviews and parent element to append to */
+	update(reviewSet, parentElement) {
+
+	}
+}
